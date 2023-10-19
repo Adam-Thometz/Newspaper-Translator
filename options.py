5e115b626e487aa6f73c34cad18d9ac01aa633d2
Adam Thometz <adamthometz@gmail.com>
2023-10-19 13:14:27 -0400

    Revert "added Egypt"
    This reverts commit 96aad833bcfafb8e0dbcb587b199219f46580d6d.

--- options.py
+++ options.py
@@ -1,4 +1,3 @@
-# Egypt - Daily News Egypt
 # Spain - El Pais
 # France - Le Monde
 
@@ -7,7 +6,6 @@
 # }
 
 country_newspapers = {
-    "Egypt": ("https://www.dailynewsegypt.com/", ".p-url"),
 	"Spain": ("https://elpais.com", ".c_t"),
-	"France": ("https://www.lemonde.fr", ".article__title-label"),
+	"France": ("https://www.lemonde.fr", ".article__title-label")
 }
