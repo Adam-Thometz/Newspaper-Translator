5b2fd0832609b31eebf1fb10d40074fead857c22
Adam Thometz <adamthometz@gmail.com>
2023-10-19 11:47:54 -0400

    added doc

--- options.py
+++ options.py
@@ -1,6 +1,10 @@
 # Spain - El Pais
 # France - Le Monde
 
+# {
+#     "country name": ("url", "classname for thing you want to scrape")
+# }
+
 country_newspapers = {
 	"Spain": ("https://elpais.com", ".c_t"),
 	"France": ("https://www.lemonde.fr", ".article__title-label")
